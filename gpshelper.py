1c88884954587618a999f5831bcecba265595f0d
mvsomewhere <mikhailvasil@gmail.com>
2021-07-02 02:25:55 +0300

    GOT GPS WORKING again

--- gpshelper.py
+++ gpshelper.py
@@ -11,13 +11,18 @@ class GpsHelper():
         gps_blinker = App.get_running_app().root.ids.mapview.ids.blinker  #it referes to root widget, which is main.kv and then it reffers to blinker widget in gpsblinker.kv        #Request permissions on android
         gps_blinker.blink()
         #request permissons on Android
-        if platform=='android':
+        if platform == 'android':
             from android.permissions import Permission, request_permissions
             def callback(permission, results):
                 if all([res for res in results]):
                     print("Got all permissions")
+                    from plyer import gps
+                    gps.configure(on_location=self.update_blinker_position,
+                                  on_status=self.on_auth_status)
+                    gps.start(minTime=1000, minDistance=0)
                 else:
                     print("did not get all permissions")
+
 
 
 
@@ -26,12 +31,10 @@ class GpsHelper():
             request_permissions([Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION], callback)
 
         #Configure GPS
-        if platform == 'android' or platform=='ios':
-            from player import gps
+        if platform == 'android' or platform =='ios':
+            from plyer import gps
             gps.configure(on_location=self.update_blinker_position, on_status=self.on_auth_status)
-            gps.configure(minTime=1000, minDistance=0)
-
-        pass
+            gps.start(minTime=1000, minDistance=0)
 
     def update_blinker_position(self, *args, **kwargs):
         my_lat= kwargs['lat']
@@ -56,8 +59,8 @@ class GpsHelper():
         else:
             self.open_gps_access_popup()
 
-    def open_gps_popup(self):
+    def open_gps_access_popup(self):
         dialog = MDDialog(title="GPS Error", text='Allow me to get your damn GPS')
         dialog.size_hint=[.8, .8]
-        dialog.pos_hint={'center_x': .5, 'center_y':.5}
+        dialog.pos_hint={'center_x': .5, 'center_y': .5}
         dialog.open()
